4dbbf23b712ff634ef2742f808ace0c7c933fa01
Nikola <nikola.shirca@gmail.com>
2024-10-30 21:43:44 -0400

    Initial commit of MovieBox

---
+++ MovieBox/src/main/java/bg/moviebox/MovieBoxApplication.java
@@ -0,0 +1,13 @@
+package bg.moviebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBoxApplication.class, args);
+	}
+
+}
