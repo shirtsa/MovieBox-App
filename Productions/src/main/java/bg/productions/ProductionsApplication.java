0cd1bdc9f59bcd47f7b51ffc87a8c212a40884c8
Nikola <nikola.shirca@gmail.com>
2024-10-30 21:56:19 -0400

    Productions initial commit

---
+++ Productions/src/main/java/bg/productions/ProductionsApplication.java
@@ -0,0 +1,13 @@
+package bg.productions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionsApplication.class, args);
+    }
+
+}
---
+++ Productions/src/test/java/bg/productions/ProductionsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.productions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
