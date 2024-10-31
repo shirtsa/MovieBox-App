64134cb35ab612d5a661361a07645e53af8b8dff
Nikola <nikola.shirca@gmail.com>
2024-10-31 12:05:29 -0400

    Add Genre enum

---
+++ MovieBox/src/main/java/bg/moviebox/model/enums/Genre.java
@@ -0,0 +1,21 @@
+package bg.moviebox.model.enums;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    ANIMATED,
+    COMEDY,
+    CRIMINAL,
+    DRAMA,
+    DOCUMENTARY,
+    FANTASY,
+    HISTORICAL,
+    HORROR,
+    MUSICAL,
+    MYSTERY,
+    ROMANCE,
+    SCIENCE_FICTION,
+    SPORTS,
+    THRILLER,
+    WESTERN,
+}
