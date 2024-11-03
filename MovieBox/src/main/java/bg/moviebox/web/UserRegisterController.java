42fc6c1de476317a5916b8deed6f4835adb9cbe3
Nikola <nikola.shirca@gmail.com>
2024-11-03 08:14:04 -0500

    Add UserRegisterController

---
+++ MovieBox/src/main/java/bg/moviebox/web/UserRegisterController.java
@@ -0,0 +1,49 @@
+package bg.moviebox.web;
+
+import bg.moviebox.model.dtos.UserRegistrationDTO;
+import bg.moviebox.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/users")
+public class UserRegisterController {
+
+    private final UserService userService;
+
+    public UserRegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @ModelAttribute("userRegistrationDTO")
+    public UserRegistrationDTO userRegistrationDTO() {
+       return new UserRegistrationDTO();
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid UserRegistrationDTO userRegistrationDTO,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("userRegistrationDTO", userRegistrationDTO);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegistrationDTO", bindingResult);
+            return "register";
+        }
+
+        userService.registerUser(userRegistrationDTO);
+
+        return "redirect:/login";
+    }
+}
