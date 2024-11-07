961a9f650312b1b0d50ec4501af200e1bb5887d0
Nikola <nikola.shirca@gmail.com>
2024-11-06 21:42:13 -0500

    Add ProductionDTO and AddProductionDTO

---
+++ Productions/src/main/java/bg/productions/model/dtos/ProductionDTO.java
@@ -0,0 +1,18 @@
+package bg.productions.model.dtos;
+
+import bg.productions.model.enums.Genre;
+import bg.productions.model.enums.ProductionType;
+
+public record ProductionDTO(
+        Long id,
+        String name,
+        String imageUrl,
+        String videoUrl,
+        Integer year,
+        Integer length,
+        Integer rating,
+        Integer rentPrice,
+        String description,
+        ProductionType productionType,
+        Genre genre) {
+}
