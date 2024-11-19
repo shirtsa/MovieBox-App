3b8110a0ad17c6c49491db7eb87315c698280c96
Nikola <nikola.shirca@gmail.com>
2024-11-19 10:17:58 -0500

    Add NewsRepository

---
+++ MovieBox/src/main/java/bg/moviebox/repository/NewsRepository.java
@@ -0,0 +1,24 @@
+package bg.moviebox.repository;
+
+import bg.moviebox.model.dtos.NewsSummaryDTO;
+import bg.moviebox.model.entities.News;
+import bg.moviebox.model.enums.NewsType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM News n WHERE n.created < :olderThan")
+    void deleteOldNews(Instant olderThan);
+
+    List<NewsSummaryDTO> findByNewsTypeOrderByIdDesc(NewsType newsType);
+}
