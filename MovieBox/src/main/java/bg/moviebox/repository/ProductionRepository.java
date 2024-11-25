4c052df5103375a867e3fe66c606ae910f0b2b86
Nikola <nikola.shirca@gmail.com>
2024-11-25 13:33:01 -0500

    Update ProductionRepository

--- MovieBox/src/main/java/bg/moviebox/repository/ProductionRepository.java
+++ MovieBox/src/main/java/bg/moviebox/repository/ProductionRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductionRepository extends JpaRepository<Production, Long> {
 
+    Production findProductionByExternalId(Long externalId);
 }
