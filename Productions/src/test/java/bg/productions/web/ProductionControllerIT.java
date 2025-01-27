09282d0fce7b6a9366aa7ff78462779cccd0d284
Nikola <nikola.shirca@gmail.com>
2025-01-27 14:06:48 -0500

    Add create production test

--- Productions/src/test/java/bg/productions/web/ProductionControllerIT.java
+++ Productions/src/test/java/bg/productions/web/ProductionControllerIT.java
@@ -5,6 +5,7 @@ import bg.productions.model.entities.Production;
 import bg.productions.model.enums.Genre;
 import bg.productions.model.enums.ProductionType;
 import bg.productions.repository.ProductionRepository;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.equalTo;
@@ -61,6 +65,55 @@ public class ProductionControllerIT {
         mockMvc.perform(get("/productions/{id}", "9999")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void testCreateProduction() throws Exception {
+
+        String uniqueName = "Alien: Romulus " + System.currentTimeMillis();
+
+        MvcResult result = mockMvc.perform(post("/productions")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "name": "%s",
+                                    "imageUrl": "https://wiziwiz.com/wp-content/uploads/2024/06/wiziwiz-alien-romulus-poster-.jpg",
+                                    "videoUrl": "x0XDEhP4MQs",
+                                    "year": 2024,
+                                    "length": 132,
+                                    "rating": 5,
+                                    "rentPrice": 10,
+                                    "description": "While scavenging the deep ends of a derelict space station, a group of young space colonizers come face to face with the most terrifying life form in the universe.",
+                                    "productionType": "MOVIE",
+                                    "genre": "HORROR"
+                                }
+                                """.formatted(uniqueName))
+                ).andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andReturn();
+
+        String body = result.getResponse().getContentAsString();
+
+        int id = JsonPath.read(body, "$.id");
+
+        Optional<Production> createdProductionOpt = productionRepository.findById((long) id);
+
+        Assertions.assertTrue(createdProductionOpt.isPresent());
+
+        Production production = createdProductionOpt.get();
+
+        Assertions.assertEquals(uniqueName, production.getName());
+        Assertions.assertEquals("https://wiziwiz.com/wp-content/uploads/2024/06/wiziwiz-alien-romulus-poster-.jpg", production.getImageUrl());
+        Assertions.assertEquals("x0XDEhP4MQs", production.getVideoUrl());
+        Assertions.assertEquals(2024, production.getYear());
+        Assertions.assertEquals(132, production.getLength());
+        Assertions.assertEquals(5, production.getRating());
+        Assertions.assertEquals(10, production.getRentPrice());
+        Assertions.assertEquals("While scavenging the deep ends of a derelict space station, a group of young space colonizers come face to face with the most terrifying life form in the universe.", createdProductionOpt.get().getDescription());
+        Assertions.assertEquals(Genre.HORROR, production.getGenre());
+        Assertions.assertEquals(ProductionType.MOVIE, production.getProductionType());
     }
 
     @Test
