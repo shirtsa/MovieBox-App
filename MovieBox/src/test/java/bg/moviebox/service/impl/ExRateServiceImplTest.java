74a5c8da698ab41358c97e054a1717be5e2687b7
Nikola <nikola.shirca@gmail.com>
2025-01-29 13:37:26 -0500

    Add test convert for rates with parameterized test

--- MovieBox/src/test/java/bg/moviebox/service/impl/ExRateServiceImplTest.java
+++ MovieBox/src/test/java/bg/moviebox/service/impl/ExRateServiceImplTest.java
@@ -1,21 +1,45 @@
 package bg.moviebox.service.impl;
 
+import bg.moviebox.config.ForexApiConfig;
+import bg.moviebox.model.entities.ExchangeRateEntity;
 import bg.moviebox.repository.ExchangeRateRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.Mock.Strictness;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ExRateServiceImplTest {
 
+  private static final class TestRates {
+    // LEV -> base
+    // CUR1 -> 4
+    // CUR2 -> 0.5
+    private static final String BASE_CURRENCY = "LEV";
+
+    private static final ExchangeRateEntity CUR1 = new ExchangeRateEntity()
+            .setCurrency("CUR1").setRate(new BigDecimal("4"));
+
+    private static final ExchangeRateEntity CUR2 = new ExchangeRateEntity()
+            .setCurrency("CUR2").setRate(new BigDecimal("0.5"));
+  }
+
   private ExchangeRateServiceImpl toTest;
 
+  /* Mockito tries to enforce strict validation, ensuring that unused stubs
+   (methods mocked but never called) cause test failures.
+   @Mock(strictness = Strictness.LENIENT) prevent parameterized tests to fail
+   where not every stubbed method is always used. */
   @Mock(strictness = Strictness.LENIENT)
   private ExchangeRateRepository mockRepository;
 
@@ -24,8 +48,42 @@ class ExRateServiceImplTest {
     toTest = new ExchangeRateServiceImpl(
         mockRepository,
         null,
-        null
+            new ForexApiConfig().setBase(TestRates.BASE_CURRENCY)
     );
+  }
+
+  // 1 LEV ->   4 CUR1
+  // 1 LEV -> 0.5 CUR2
+  @ParameterizedTest(name = "Converting {2} {0} to {1}. Expected {3}")
+  @CsvSource(
+          textBlock = """
+          LEV, CUR1, 1, 4.00
+          LEV, CUR1, 2, 8.00
+          LEV, LEV,  1, 1
+          CUR1,CUR2, 1, 0.12
+          CUR2,CUR1, 1, 8.00
+          EUR, EUR, 1, 1
+          """
+  )
+  void testConvert(String from,  //Argument 0
+                   String to, //Argument 1
+                   BigDecimal amount, //Argument 2
+                   BigDecimal expected)  { //Argument 3
+
+//    System.out.println(from + " " + to + " " + amount + " " + expected);
+
+    initExRates();
+
+    BigDecimal result = toTest.convert(from, to, amount);
+    Assertions.assertEquals(expected, result);
+  }
+
+
+  private void initExRates() {
+    when(mockRepository.findByCurrency(TestRates.CUR1.getCurrency()))
+            .thenReturn(Optional.of(TestRates.CUR1));
+    when(mockRepository.findByCurrency(TestRates.CUR2.getCurrency()))
+            .thenReturn(Optional.of(TestRates.CUR2));
   }
 
   @Test
